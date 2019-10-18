38258db3bb7a83656d52589ce1486aa11f8d8faf
Mateusz Grabiec <frotek@me.com>
2019-10-18 11:17:53 +0200

    Add winning, losing, wrong number

--- js/script.js
+++ js/script.js
@@ -26,6 +26,26 @@ if(playerInput == '1'){
   playerMove = 'nożyce';
 } else if(playerInput == '3'){
   playerMove = 'papier';
+} else if(playerInput => 4){
+  printMessage('Liczby tylko od 1-3')
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!')
+} else if(computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!')
+} else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!')
+} else if(computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Komputer wygrywa!')
+} else if(computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Komputer wygrywa!')
+} else if(ComputerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Komputer wygrywa!')
+}
+
+if (computerMove == playerMove) {
+  printMessage('REMIS !!')
+}
